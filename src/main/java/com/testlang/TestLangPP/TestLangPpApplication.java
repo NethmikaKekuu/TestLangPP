f25533fdd30ab44795da90cc3b343899d7290b20
IT23657496NethmikaKeku <it23657496@my.sliit.lk>
2025-10-22 20:33:06 +0530

    Initial maven project with dependencies

---
+++ src/main/java/com/testlang/TestLangPP/TestLangPpApplication.java
@@ -0,0 +1,13 @@
+package com.testlang.TestLangPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLangPpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLangPpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testlang/TestLangPP/TestLangPpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testlang.TestLangPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLangPpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
